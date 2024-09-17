70d741d44b042d6d0ae3d3435b5ff33defc8f67f
Miri Markovitz <mirimarkovitzbl@gmail.com>
2024-09-17 13:33:18 -0700

    Fix some console errors

--- app/components/TechSkill.jsx
+++ app/components/TechSkill.jsx
@@ -7,8 +7,8 @@ const TechSkill = ({ src, alt, title, className }) => {
       className={`rounded-xl text-white bg-gradient-to-br from-[#DC2424] to-[#4A569D] p-1 
     grid col-span-2 row-span-2 ${className}`}
     >
-      <div className="rounded-xl bg-[#252526] py-2 flex flex-col items-center w-32 h-24 ">
-        <Image className="" src={src} alt={alt} width={50} height={50} />
+      <div className="rounded-xl bg-[#252526] py-3 flex flex-col items-center w-32 h-24 ">
+        <Image className="w-auto h-auto" src={src} alt={alt} width={40} height={40} />
         <p className="pt-2 text-xs text-center font-semibold">{title}</p>
       </div>
     </div>
